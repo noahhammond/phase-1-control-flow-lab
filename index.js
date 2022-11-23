9520df69f34e51c9f94ed30de69191128a799643
Noah <business.noah@hotmail.com>
2022-11-23 13:54:01 -0700

    commit tree b0f31d6d82b1bada820299c38822047e4041ab79

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (distance >= 2501) {
+    return 'No can do.'
+  }
+}
+scuberGreetingForFeet(199);
+
+function ternaryCheckCity(bigCity){
+  const bigCityMessage = (bigCity === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+  return bigCityMessage
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let reply;
+
+if (tip === 'generous') {
+  return reply = "Thank you so much.";
+} else if (tip === 'not as generous') {
+  return reply = 'Thank you.'
+} else {
+  return reply = 'Bye.';
+}
 }
